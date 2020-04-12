e6fcc95754c55d00c5ca538bf4fa4b7bbd08e62b
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-04-12 19:11:59 +0400

    add updating post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const { validationResult } = require('express-validator/check');
 
 const Post = require('../models/post');
@@ -34,7 +37,7 @@ exports.createPost = (request, response, next) => {
 
 	const { title, content } = request.body;
 	const { file } = request;
-	const imageUrl = file.path.replace("\\" ,"/");
+	const imageUrl = file.path.replace('\\', '/');
 
 	const post = Post({
 		title,
@@ -77,4 +80,67 @@ exports.getPost = (request, response, next) => {
 	.catch((error) => {
 		next(error);
 	});
+};
+
+exports.updatePost = (request, response, next) => {
+	const errors = validationResult(request);
+
+	if (!errors.isEmpty()) {
+		const error = new Error('Validation failed, entered data is incorrect.');
+		error.statusCode = 422;
+
+		throw error;
+	}
+
+	const { params, body, file } = request;
+	const { postId } = params;
+	const { title, content, image } = body;
+	let imageUrl = image;
+
+	if (request.file) {
+		imageUrl = file.path.replace('\\', '/');
+	}
+
+	if (!imageUrl) {
+		const error = new Error('No file picked.');
+		error.statusCode = 422;
+
+		throw error;
+	}
+
+	Post.findById(postId)
+	.then((post) => {
+		if (!post) {
+			const error = new Error('Could not find post!');
+			error.status(404);
+
+			throw error;
+		}
+
+		if (imageUrl !== post.imageUrl) {
+			clearImage(post.imageUrl);
+		}
+
+		post.title = title;
+		post.imageUrl = imageUrl;
+		post.content = content;
+
+		return post.save();
+	})
+	.then((post) => {
+		response.status(200).json({
+			message: 'Post updated!',
+			post,
+		})
+	})
+	.catch((error) => {
+		next(error);
+	});
+};
+
+const clearImage = (filePath) => {
+	filePath = path.join(__dirname, '..', filePath);
+	fs.unlink(filePath, (error) => {
+		console.log(error);
+	})
 };
