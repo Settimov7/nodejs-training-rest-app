1e28e97819bd710906846435bce4afc68c5b02ac
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-04-12 21:34:29 +0400

    add creating jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,11 @@
 const { validationResult } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
 
 const User = require('../models/user');
+
+dotenv.config();
 
 exports.signup = (request, response, next) => {
 	const errors = validationResult(request);
@@ -18,19 +22,64 @@ exports.signup = (request, response, next) => {
 
 	bcrypt.hash(password, 12)
 	.then((password) => {
-			const user = new User({
-				email,
-				password,
-				name,
-			});
+		const user = new User({
+			email,
+			password,
+			name,
+		});
 
-			return user.save();
+		return user.save();
 	})
 	.then((user) => {
 		response.status(201).json({
 			message: 'User created!',
 			userId: user._id,
 		});
+	})
+	.catch((error) => {
+		next(error);
+	})
+};
+
+exports.login = (request, response, next) => {
+	const { email, password } = request.body;
+	let currentUser;
+
+	User.findOne({ email })
+	.then((user) => {
+		if (!user) {
+			const error = new Error('A user with this email could not be found.');
+			error.statusCode = 401;
+
+			throw error;
+		}
+
+		currentUser = user;
+
+		return bcrypt.compare(password, user.password);
+	})
+	.then((isEqual) => {
+		if (!isEqual) {
+			const error = new Error('Wrong password.');
+			error.statusCode = 401;
+
+			throw error;
+		}
+
+		const { _id, email } = currentUser;
+		const userId = _id.toString();
+		const token = jwt.sign(
+			{
+				email,
+				userId,
+			},
+			process.env.SECRET_KEY,
+			{
+				expiresIn: '1h',
+			},
+		);
+
+		response.status(200).json({ token, userId })
 	})
 	.catch((error) => {
 		next(error);
