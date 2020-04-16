c4754f356c669c354e1f552567d7e491c0422db5
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-04-16 19:11:31 +0400

    add socket.io

--- app.js
+++ app.js
@@ -62,7 +62,12 @@ app.use((error, request, response, next) => {
 
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
-	app.listen(8080);
+	const server = app.listen(8080);
+	const io = require('socket.io')(server);
+
+	io.on('connection', (socket) => {
+		console.log('Client connected')
+	})
 })
 .catch((error) => {
 	console.log(error);
